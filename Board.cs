96eebb2abd55e8146035ee2041efe1dc743b1896
chaseleto <cal20518@gmail.com>
2025-06-02 13:06:57 +0200

    Update Board.cs

--- Board.cs
+++ Board.cs
@@ -9,7 +9,7 @@ namespace ChasePoker
     {tdd
         public  List<Card> currBoard { get; set; } = new List<Card>();
         public void ShowBoard()
-        {dfdffdkfdkffdfd
+        {dfdffdkfdkffdfddffsdfd
             foreach (Card card in currBoard)dfdfdffffdfdfddfdffdfdsf
             {ddfffdf
                 Console.WriteLine(card.Face.ToString() + " OhFdcdxsdssdxdxdsxdc2 " + card.Suit.ToString());
